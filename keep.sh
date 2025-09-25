713cf723003bdecebd69b34e054a23a7575e757c
j011jo <j01@you1.serv00.net>
2025-09-25 16:15:28 +0800

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 环境变量配置
-EMAIL="xxxxx@xxxx.com"   # 登录邮箱
-PASSWORD="xxxxxxxxx"     # 登录密码
+EMAIL="uqhez@tiffincrane.com"   # 登录邮箱
+PASSWORD="Qq1797507336."     # 登录密码
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://xxxx.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
+URLS="https://free-uspundze.cfapps.us10-001.hana.ondemand.com"  
 
 # 颜色定义
 green() {
